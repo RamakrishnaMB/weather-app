86c8a8bc0d471a6dbd2477edca6ee951757d1dde
ramakrishna <ramakrishnamb@gmail.com>
2024-05-01 19:11:02 +0530

    rendering all data in correct format

--- src/WeatherComponent.tsx
+++ src/WeatherComponent.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import React, {useState, useEffect} from 'react';
+import {Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper} from '@mui/material';
 
 interface WeatherData {
     location: {
@@ -34,18 +34,28 @@ const WeatherComponent: React.FC = () => {
                         const jsonData = await response.json();
                         const country = jsonData.location.country;
                         const date: string = jsonData.forecast.forecastday[0].date; // Specify the type of date as string
-                        return { country, data: jsonData, date };
+                        return {country, data: jsonData, date};
                     })
                 );
 
                 // Group data by date
-                const groupedData: { [key: string]: { date: string; countries: { country: string; data: WeatherData }[] } } = {};
-                fileContents.forEach(({ country, data, date }) => {
-                    data.forecast.forecastday.forEach(({ date: forecastDate, hour }: { date: string; hour: { time: string; condition: { icon: string; text: string; }; temp_c: number; temp_f: number; }[] }) => { // Specify the type of date and hour
+                const groupedData: {
+                    [key: string]: { date: string; countries: { country: string; data: WeatherData }[] }
+                } = {};
+                fileContents.forEach(({country, data, date}) => {
+                    data.forecast.forecastday.forEach(({date: forecastDate, hour}: {
+                        date: string;
+                        hour: {
+                            time: string;
+                            condition: { icon: string; text: string; };
+                            temp_c: number;
+                            temp_f: number;
+                        }[]
+                    }) => { // Specify the type of date and hour
                         if (!groupedData[forecastDate]) { // Change variable name from date to forecastDate
-                            groupedData[forecastDate] = { date: forecastDate, countries: [] }; // Change variable name from date to forecastDate
+                            groupedData[forecastDate] = {date: forecastDate, countries: []}; // Change variable name from date to forecastDate
                         }
-                        groupedData[forecastDate].countries.push({ country, data });
+                        groupedData[forecastDate].countries.push({country, data});
                     });
                 });
 
@@ -60,35 +70,37 @@ const WeatherComponent: React.FC = () => {
 
     const formatTime = (time: string) => {
         const date = new Date(time);
-        return date.toLocaleString('en-US', { hour: 'numeric', hour12: true });
+        return date.toLocaleString('en-US', {hour: 'numeric', hour12: true});
     };
 
     return (
         <div>
-            {data.map(({ date, countries }) => (
+            {data.map(({date, countries}) => (
                 <div key={date}>
                     <Typography variant="h5">{date}</Typography>
-                    {countries.map(({ country, data }) => (
-                        <Paper key={`${country}-${date}`} style={{ marginBottom: '20px', padding: '20px' }}>
-                            <Typography variant="subtitle1">Country: {country}</Typography>
-                            <TableContainer component={Paper} style={{ marginTop: '20px' }}>
+                    {countries.map(({country, data}) => (
+                        <Paper key={`${country}-${date}`} style={{marginBottom: '20px', padding: '20px'}}>
+                            <TableContainer component={Paper} style={{marginTop: '20px'}}>
                                 <Table>
                                     <TableHead>
                                         <TableRow>
-                                            <TableCell>Time</TableCell>
-                                            <TableCell>Icon & Temperature (&#176;C / &#176;F)</TableCell>
+                                            <TableCell>Country</TableCell>
+                                            {data.forecast.forecastday.find(forecast => forecast.date === date)?.hour.filter((hour, index) => index % 3 === 0).map(hour => (
+                                                <TableCell>{formatTime(hour.time)}</TableCell>
+                                            ))}
                                         </TableRow>
                                     </TableHead>
                                     <TableBody>
-                                        {data.forecast.forecastday.find(forecast => forecast.date === date)?.hour.filter((hour, index) => index % 3 === 0).map(hour => (
-                                            <TableRow key={hour.time}>
-                                                <TableCell>{formatTime(hour.time)}</TableCell>
+                                        <TableRow>
+                                            <TableCell>{country}</TableCell>
+                                            {data.forecast.forecastday.find(forecast => forecast.date === date)?.hour.filter((hour, index) => index % 3 === 0).map(hour => (
                                                 <TableCell>
-                                                    <img src={hour.condition.icon} alt={hour.condition.text} style={{ maxWidth: '50px', marginRight: '10px' }} />
+                                                    <img src={hour.condition.icon} alt={hour.condition.text}
+                                                         style={{maxWidth: '50px', marginRight: '10px'}}/>
                                                     {hour.temp_c}&#176;C / {hour.temp_f}&#176;F
                                                 </TableCell>
-                                            </TableRow>
-                                        ))}
+                                            ))}
+                                        </TableRow>
                                     </TableBody>
                                 </Table>
                             </TableContainer>
@@ -98,6 +110,7 @@ const WeatherComponent: React.FC = () => {
             ))}
         </div>
     );
+
 };
 
 export default WeatherComponent;
